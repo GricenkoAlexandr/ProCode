bd908fc7e118697258389d2baccd014e4a91b0cc
Alexandr <alexgriko29@gmail.com>
2021-04-03 21:47:53 +0300

    task2l1

--- ProJS2/js/script.js
+++ ProJS2/js/script.js
@@ -1,12 +1,3 @@
+`use strict`;
 
-let inp1=document.querySelector('.inp1');
-let inp2=document.querySelector('.inp2');
-let inpbtn=document.querySelector('.btn1');
-let rez=document.querySelector('.rez');
 
-inpbtn.addEventListener('click', ()=> {
-    let ind=Number(inp1.value);
-    let ind2=Number(inp1.value);
-    rezult=Math.pow(ind, ind2);
-    rez.innerHTML=rezult;
-})
