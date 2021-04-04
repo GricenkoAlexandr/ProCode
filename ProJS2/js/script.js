09045d37df361016f0ebdb6871f9721535f570c9
Alexandr <alexgriko29@gmail.com>
2021-04-04 21:40:37 +0300

    Projs2l1

--- ProJS2/js/script.js
+++ ProJS2/js/script.js
@@ -1,3 +1,17 @@
 `use strict`;
 
+let outfn=document.querySelector('.out1');
+let outmn=document.querySelector('.out2');
+let outln=document.querySelector('.out3');
 
+const form=document.querySelector('.input_block');
+
+form.addEventListener('submit', (ev)=>{
+    ev.preventDefault();
+    let inpf1=document.querySelector('.inp1').value;
+    let inpf2=document.querySelector('.inp2').value;
+    let inpf3=document.querySelector('.inp3').value;
+    outfn.innerHTML=inpf1;
+    outmn.innerHTML=inpf2;
+    outln.innerHTML=inpf3;
+});
