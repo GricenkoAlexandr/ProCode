f4ba395fc675c565300a3c8710ae4cc15d420f1a
Alexandr <alexgriko29@gmail.com>
2021-04-14 22:49:22 +0300

    add background and make it beutifull

--- ProJS5/js/script.js
+++ ProJS5/js/script.js
@@ -1,36 +1,48 @@
- `use strict`;
+`use strict`;
 
  let arClass=[
     { name: 'Александр', surname: 'Гриценко', task: 3 },
     { name: 'Иванна', surname: 'Зимина', task: 5 },
     { name: 'Александр', surname: 'Гетьманский', task: 9 },
     { name: 'Наталья', surname: 'Горошко', task: 7 },
-    { name: 'Артем', surname: 'Шевчук', task: 3 }
+    { name: 'Andrew', surname: 'Stehnii', task: 3 },
+    { name: 'Инна', surname: 'Олещенко', task: 8 },
+    { name: 'Николай', surname: 'Зайченко', task: 9 },
+    { name: 'Denis', surname: 'Rudik', task: 5 },
+    { name: 'Andrey', surname: 'Timoshenko', task: 7 },
+    { name: 'Артем', surname: 'Шевчук', task: 3 },
  ];
 
-const btn1=document.querySelector('.out1_btn');
-const btn2=document.querySelector('.out2_btn');
-const out1=document.querySelector('.out-1');
-const out2=document.querySelector('.out-2');
+const btnAll=document.querySelector('.out1_btn');
+const btnTask=document.querySelector('.out2_btn');
+const outAll=document.querySelector('.out-1');
+const outTask=document.querySelector('.out-2');
 
-btn1.addEventListener('click', ()=>{
+btnAll.addEventListener('click', ()=>{
     let out11=[];
     for (let i=0; i<arClass.length; i++){
         out11+=`${i+1}. Cтудент ${arClass[i]['surname']} ${arClass[i]['name']} сделал ${arClass[i]['task']} заданий </br>`;
     }
 
-    out1.innerHTML=out11;
+    outAll.innerHTML=out11;
 });
 
-btn2.addEventListener('click', ()=>{
+btnTask.addEventListener('click', ()=>{
+
     let out12=[];
+    let taskmax=9;
     let tasq=document.querySelector('.taskqua').value;
-    let arClassfilter=arClass.filter((tas)=> {
-        return tas.task>=tasq;
-    })
-    for (let i=0; i<arClassfilter.length; i++){
-        out12+=`${i+1}. Cтудент ${arClassfilter[i]['surname']} ${arClassfilter[i]['name']} сделал ${arClassfilter[i]['task']} заданий </br>`;
+    if (tasq>taskmax){
+        out12='Чёто как-то многовато заданий походу))';
+    } else {
+        let arClassfilter=arClass.filter((tas)=> {
+            return tas.task>=tasq;
+        })
+        for (let i=0; i<arClassfilter.length; i++){
+            out12+=`${i+1}. Cтудент ${arClassfilter[i]['surname']} ${arClassfilter[i]['name']} сделал ${arClassfilter[i]['task']} заданий </br>`;
+        }
     }
-       out2.innerHTML=out12;
-});
 
+    
+    outTask.innerHTML=out12;
+});
