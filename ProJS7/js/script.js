22cd96d9b30d051098911721edbf56457f7d921a
Alexandr <alexgriko29@gmail.com>
2021-04-24 19:34:43 +0300

    add task 7

---
+++ ProJS7/js/script.js
@@ -0,0 +1,32 @@
+`use strict`;
+
+const startBtn=document.querySelector('.startbtn');
+
+startBtn.addEventListener('click', ()=> {
+    const rowsBtn=document.querySelector('.rbtn').value;
+    const colBtn=document.querySelector('.cbtn').value;
+    const outBlock=document.querySelector('.out_block');
+    const displ=document.querySelector('.manager');
+    let cols='';
+    for (let i=0;i<colBtn;i++) {
+        cols+='<br>';
+        for (let j=0;j<rowsBtn;j++) {
+        cols+=`<div class="rows"></div>`;
+    }
+    }
+    outBlock.innerHTML=cols;
+    displ.classList.remove('hide');
+});
+
+
+/*
+const btns={
+    {name: 'up', state: true},
+    {name: 'right', state: true},
+    {name: 'down', state: true},
+    {name: 'left', state: true},
+}
+*/
+
+
+
